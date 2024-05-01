db3f7066af700ffa1a239b5843cb7c9c95b3fe50
Phyrunez <ebukajudee302@gmail.com>
2024-05-01 22:21:31 +0100

    worked on the user profile page

---
+++ src/components/ProfilePicture.jsx
@@ -0,0 +1,92 @@
+import { useState } from 'react'
+import CropEasy from './crop/CropEasy'
+import { stables } from '../constants'
+import { HiOutlineCamera } from "react-icons/hi"
+import { useDispatch, useSelector } from 'react-redux'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { updateProfilePicture } from '../services/users'
+import { userActions } from '../store/reducers/userReducer'
+import toast from 'react-hot-toast'
+import { createPortal } from 'react-dom'
+
+const ProfilePicture = ({ avatar }) => {
+  const [openCrop, setOpenCrop] = useState(false)
+  const [photo, setPhoto] = useState(null)
+
+  const dispatch = useDispatch();
+  const queryClient = useQueryClient()
+  const userState = useSelector(state => state.user)
+
+  const handleFileChange = (e) => {
+    const file = e.target.files[0]
+    if(e.target.files.length !== 0) setPhoto({ url: URL.createObjectURL(file), file})
+    setOpenCrop(true)
+  }
+
+  const { mutate, isLoading } = useMutation({
+    mutationFn: ({ token, formData }) => {
+        return updateProfilePicture({
+            token: token,
+            formData: formData
+         })
+    },
+    onSuccess: (data) => {
+        dispatch(userActions.setUserInfo(data));
+        setOpenCrop(false)
+        localStorage.setItem("user-account", JSON.stringify(data));
+        queryClient.invalidateQueries(["profile"]);
+        toast.success("Profile photo is Removed")
+    },
+    onError: (error) => {
+        toast.error(error.message)
+    }
+  })
+
+  const handleDeleteImage = () => {
+    const warningMsg = createPortal(window.confirm("Do you want to delete your profile picture?"), document.getElementById('portal'))
+    if(warningMsg) {
+        try {
+            const formData = new FormData()
+            formData.append("profilePicture", undefined)
+
+            mutate({ token: userState.userInfo.token, formData: formData })
+        } catch (error) {
+            toast.error(error.message)
+        }
+    }
+  }
+ 
+  return (
+    <>
+        { openCrop && createPortal(<CropEasy photo={photo} setOpenCrop={setOpenCrop} />, document.getElementById('portal')) }
+
+        <div className='w-full flex items-center gap-x-4'>
+            <div className='relative w-20 h-20 rounded-full  outline outline-offset-2 outline-1 outline-[#025750] overflow-hidden'>
+                <label htmlFor="profilePicture" className='cursor-pointer absolute inset-0 rounded-full bg-transparent'>
+                    {avatar ? (
+                        <img 
+                            src={stables.UPLOAD_FOLDER_BASE_URL + avatar} 
+                            alt="avatar" 
+                            className='w-full h-full object-cover' 
+                        />
+                    ) : (
+                        <div className='h-full w-full bg-blue-50/50 flex justify-center items-center'>
+                            <HiOutlineCamera className='w-7 h-auto text-[#025750]' />
+                        </div>
+                    )}
+                </label>
+                <input type="file" className='sr-only' id="profilePicture" onChange={handleFileChange} />
+            </div>
+            <button 
+                onClick={handleDeleteImage}
+                type="button" 
+                className='border border-red-500 rounded-md text-red-500 px-2 py-1 hover:bg-red-500 hover:text-white'
+            >
+                Delete
+            </button>
+        </div>
+    </>
+  )
+}
+
+export default ProfilePicture
--- src/pages/profile/ProfilePage.jsx
+++ src/pages/profile/ProfilePage.jsx
@@ -1,8 +1,149 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
+import MainLayout from '../../components/MainLayout'
+import ProfilePicture from '../../components/ProfilePicture'
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
+import { getUserProfile, updateProfile } from '../../services/users'
+import { useDispatch, useSelector } from 'react-redux'
+import { useForm } from 'react-hook-form'
+import { useNavigate } from 'react-router-dom'
+import { userActions } from '../../store/reducers/userReducer'
+import toast from 'react-hot-toast'
 
 const ProfilePage = () => {
+  const [nameVal, setNameVal] = useState("")
+  const [emailVal, setEmailVal] = useState("")
+  const [passwordVal, setPasswordVal] = useState("")
+
+  const dispatch = useDispatch()
+  const userState = useSelector(state => state.user)
+  const navigate = useNavigate()
+  const queryClient = useQueryClient()
+
+  const { data: profileData, isPending: profileIsPending, error } = useQuery({
+    queryFn: () => {
+      return getUserProfile({ token: userState.userInfo?.token })
+    },
+    queryKey: ['profile']
+  })
+
+  const { mutate, isLoading: updateProfileIsLoading } = useMutation({
+    mutationFn: ({ name, email, password }) => {
+      return updateProfile({
+        token: userState.userInfo.token,
+        userData: { name, email, password }
+      })
+    },
+    onSuccess: (data) => {
+      dispatch(userActions.setUserInfo(data));
+      localStorage.setItem("account", JSON.stringify(data));
+      queryClient.invalidateQueries(["profile"]);
+      toast.success("Profile updated successfully")
+    },
+    onError: (error) => {
+      toast.error(error.message)
+    }
+  })
+
+  const { register, handleSubmit, formState: { errors, isValid }} = useForm({
+    defaultValues: {
+      name: "",
+      email: "",
+      password: ""
+    },
+    values: {
+      name: profileIsPending ? " " : profileData.name,
+      email: profileIsPending ? " " : profileData.email,
+    },
+    mode: 'onChange'
+  })
+
+  const submitHandler = (data) => {
+    const { name, email, password } = data
+    mutate({ name, email, password })
+  }
+
+  useEffect(() => {
+    if(!userState.userInfo) navigate("/")
+  }, [navigate, userState.userInfo])
+
   return (
-    <div>ProfilePage</div>
+    <MainLayout>
+      <section className="container mx-auto px-5 py-10">
+        <div className="w-full max-w-sm mx-auto">
+          <ProfilePicture avatar={ profileData?.avatar } />
+          <form onSubmit={handleSubmit(submitHandler)} className='mt-6'>
+            <div className="flex flex-col mb-6 w-full">
+              <label htmlFor="name" className='text-[#5a7184] font-semibold block'>Name</label>
+              <input 
+                type="text" 
+                id="name" 
+                {...register("name", {
+                    minLength: { value: 1, message: "Name length must be greater than 1 character"},
+                    required: { value: true, message: "Name field is required!"},
+                })} 
+                onChange={e => setNameVal(e.target.value)}
+                placeholder="Enter Name"
+                className={`placeholder:text-[#959ead] text-dark-hard mt-3 rounded-lg px-5 py-4 font-semibold block
+                outline-none border ${errors.name ? "border-red-500" : "border-[#c3cad9]"} ${nameVal && "border-green-500"}`} 
+              />
+
+              {errors.name?.message &&
+                  <p className='text-red-500 text-xs mt-1'>{ errors?.name.message }</p>
+              }
+                
+            </div>
+
+            <div className="flex flex-col mb-6 w-full">
+              <label htmlFor="email" className='text-[#5a7184] font-semibold block'>Email Address</label>
+              <input 
+                type="email" 
+                id="email" 
+                {...register("email", {
+                    pattern: { 
+                        value: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, 
+                        message: "Enter a valid Email address"
+                    },
+                    required: { value: true, message: "Email address field is required!"}
+                })} 
+                onChange={e => setEmailVal(e.target.value)}
+                placeholder="Enter Email"
+                className={`placeholder:text-[#959ead] text-dark-hard mt-3 rounded-lg px-5 py-4 font-semibold block
+                outline-none border ${errors.email ? "border-red-500" : "border-[#c3cad9]"} ${emailVal.includes('@') && "border-green-500"}`} 
+              />
+
+              {errors.email?.message && (
+                  <p className='text-red-500 text-xs mt-1'>{ errors?.email.message }</p>
+              )}
+            </div>
+
+            <div className="flex flex-col mb-6 w-full">
+                <label htmlFor="password" className='text-[#5a7184] font-semibold block'>New Password (optional)</label>
+                <input 
+                  type="password" 
+                  id="password" 
+                  {...register("password")} 
+                  onChange={e => setPasswordVal(e.target.value)}
+                  placeholder="Enter New Password"
+                  className={`placeholder:text-[#959ead] text-dark-hard mt-3 rounded-lg px-5 py-4 font-semibold block
+                  outline-none border ${errors.password ? "border-red-500" : "border-[#c3cad9]"} ${passwordVal.length >= 6 && "border-green-500"}`} 
+                />
+
+                {errors.password?.message && (
+                    <p className='text-red-500 text-xs mt-1'>{ errors?.password.message }</p>
+                )}
+            </div>
+
+            <button 
+              type='submit' 
+              disabled={passwordVal === "" || passwordVal.length < 6 || !isValid || profileIsPending || updateProfileIsLoading}
+              className='bg-[#025750] text-white font-bold text-lg py-4 w-full rounded-lg my-6 disabled:opacity-70 disabled:cursor-not-allowed'
+            >
+              Update 
+            </button>
+          </form>
+        </div>
+      </section>
+    </MainLayout>
   )
 }
 
