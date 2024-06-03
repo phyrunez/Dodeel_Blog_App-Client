609ca2237f6a389098ebd1b776ec5abd1ac5a15f
Phyrunez <ebukajudee302@gmail.com>
2024-06-03 20:16:18 +0100

    made changes

--- src/services/posts.js
+++ src/services/posts.js
@@ -1,11 +1,13 @@
 import axios from "axios";
 
+const API_URL = "https://dodeel-blog-app-server.onrender.com"
+
 export const addPost = async({ title, author, introText, supportText, italicText,
     summaryText, mainPhoto, supportPhoto, category, date, likes }) => 
 {
     try {
         
-        const { data } = await axios.post('/api/posts', {
+        const { data } = await axios.post(`${API_URL}/api/posts`, {
             title, author, introText, supportText, italicText,
             summaryText, mainPhoto, supportPhoto, category, date, likes
         })
@@ -19,7 +21,7 @@ export const addPost = async({ title, author, introText, supportText, italicText
 
 export const getAllPosts = async(searchKeyword = "", page = 1, limit = 10) => {
     try {
-        const  { data, headers } = await axios.get(`/api/posts/?searchKeyword=${searchKeyword}&page=${page}&limit=${limit}`)
+        const  { data, headers } = await axios.get(`${API_URL}/api/posts/?searchKeyword=${searchKeyword}&page=${page}&limit=${limit}`)
         console.log({ data, headers })
         return { data, headers }
     } catch (error) {
@@ -30,7 +32,7 @@ export const getAllPosts = async(searchKeyword = "", page = 1, limit = 10) => {
 
 export const getSinglePost = async({ slug }) => {
     try {
-        const { data } = await axios.get(`/api/posts/${slug}`)
+        const { data } = await axios.get(`${API_URL}/api/posts/${slug}`)
         console.log(data)
         return data
     } catch (error) {
@@ -41,7 +43,7 @@ export const getSinglePost = async({ slug }) => {
 
 export const deleteSinglePost = async({ slug }) => {
     try {
-        const { data } = await axios.delete(`/api/posts/${slug}`);
+        const { data } = await axios.delete(`${API_URL}/api/posts/${slug}`);
         
         return data;
     } catch (error) {
@@ -53,7 +55,7 @@ export const deleteSinglePost = async({ slug }) => {
 
 export const updatePost = async({ slug, userData }) => {
     try {
-        const { data } = await axios.put(`/api/posts/${slug}`, userData);
+        const { data } = await axios.put(`${API_URL}/api/posts/${slug}`, userData);
         
         return data;
     } catch (error) {
@@ -65,7 +67,7 @@ export const updatePost = async({ slug, userData }) => {
 
 export const likePost = async({ slug }) => {
     try {
-        const { data } = await axios.post(`/api/posts/${slug}/like`);
+        const { data } = await axios.post(`${API_URL}/api/posts/${slug}/like`);
         console.log(slug)
         return data;
     } catch (error) {
