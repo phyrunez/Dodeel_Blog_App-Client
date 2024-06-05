f0c0bbf1fb08e7d943df3c2fc93eea04184ce93a
Phyrunez <ebukajudee302@gmail.com>
2024-06-05 14:47:30 +0100

    made changes

--- src/pages/home/container/Articles.jsx
+++ src/pages/home/container/Articles.jsx
@@ -29,10 +29,10 @@ const Articles = () => {
     queryFn: () => getAllPosts(searchKeyword, currentPage)
   })
 
-  // useEffect(() => {
+  useEffect(() => {
     let data = getAllPosts(searchKeyword, currentPage)
     console.log(data)
-  // }, [])
+  }, [])
 
   console.log(postsData)
   console.log(activeNavName)
