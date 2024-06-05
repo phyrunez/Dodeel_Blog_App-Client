7ff13f9a81d5195a38fad02983b03a27628857a9
Phyrunez <ebukajudee302@gmail.com>
2024-06-05 13:11:09 +0100

    made changes

--- src/services/posts.js
+++ src/services/posts.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = 'https://dodeel-blog-app-server.onrender.com/'
+const API_URL = 'https://dodeel-blog-app-server.onrender.com'
 console.log(API_URL)
 
 export const addPost = async({ title, author, introText, supportText, italicText,
